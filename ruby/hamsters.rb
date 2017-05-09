b0e8e0089fef8f12cedd11a48cfa1c23d1dc41fe
genoth <nothgwynne@gmail.com>
2017-05-09 14:54:26 -0500

    Correct string and integer data and add user feedback

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,4 +1,4 @@
-puts "What is the hamster's name?"
+puts "Let's register a hamster! What is the hamster's name?"
 hamster_name = gets.chomp
 puts "How loud is the hamster on a scale of 1 to 10, with 10 being loudest?"
 loudness = gets.chomp.to_i
@@ -11,6 +11,12 @@ if adoption == ""
 end
 puts "What is the hamster's age (approximately)?"
 age = gets.chomp.to_i
-if age == ""
+if age == 0
   age = "nil"
 end
+puts "Thank you for registering this hamster."
+puts "Hamster name: #{hamster_name}"
+puts "Hamster volume: #{loudness}"
+puts "Hamster color: #{fur_color}"
+puts "Candidate for adoption: #{adoption}"
+puts "Approximate age: #{age}"
