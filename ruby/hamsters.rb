8e61e51295c685ce25d4842c70ba04db7f6f50e5
genoth <nothgwynne@gmail.com>
2017-05-09 14:34:56 -0500

    Complete initial code for Release 1

---
+++ ruby/hamsters.rb
@@ -0,0 +1,8 @@
+puts "What is the hamster's name?"
+hamster_name = gets.chomp
+puts "How loud is the hamster on a scale of 1 to 10, with 10 being loudest?"
+loudness = gets.chomp.to_i
+puts "What is the hamster's fur color?"
+fur_color = gets.chomp
+puts "Is the hamster a good candidate for adoption? Answer yes or no."
+adoption = gets.chomp
