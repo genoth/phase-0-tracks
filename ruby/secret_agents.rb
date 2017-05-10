e8ca040847da2a1fa89489e104d1db37c2aa22ac
genoth <nothgwynne@gmail.com>
2017-05-10 15:28:55 -0500

    Complete pseudocode for Release 5 and begin coding

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -32,22 +32,40 @@ p encrypt("string")
 # decrypt by retracting by 1 letter for each character, using our key
 # returns the decrypted string
 
-def decrypt(tusjoh)
+def decrypt(string)
   our_index = 0
   key = "abcdefghijklmnopqrstuvwxyz"
-  while our_index < tusjoh.length do
-    tusjoh[our_index] = key[key.index(tusjoh[our_index]) - 1]
+  while our_index < string.length do
+    string[our_index] = key[key.index(string[our_index]) - 1]
     our_index = our_index + 1
   end
-  p tusjoh
+  p string
 end
 
-decrypt("tusjoh")
+# decrypt("tusjoh")
 
-encrypt("abc")
-encrypt("zed")
-decrypt("bcd")
-decrypt("afe")
+# encrypt("abc")
+# encrypt("zed")
+# decrypt("bcd")
+# decrypt("afe")
 
-decrypt(encrypt("swordfish"))
+# decrypt(encrypt("swordfish"))
+# Release 4
 # This nested method call works because it is asking to encrypt "swordfish." Encrypt returns a string that decrypt reads. The result is that the original string is returned.
+
+# Release 5
+# Print question to ask user if they'd like to use the decrypt or encrypt method.
+# use gets.chomp to take the user input
+# Ask user for the word they want us to encrypt or decrypt.
+# Perform the operation and print the result, then say thank you for using the cryptometer!
+
+puts "Hello Agent! Would you like to use the encrypt method, or the decrypt method? Please type encrypt or decrypt."
+method = gets.chomp
+until method == "encrypt" || "decrypt"
+  puts "Invalid input. Please try again. Type either encrypt or decrypt."
+  method = gets.chomp
+end
+puts "What word would you like to use?"
+
+
+
