2f265b09176e7175e06d45c7775bf2a20a880bb0
genoth <nothgwynne@gmail.com>
2017-05-10 17:00:59 -0500

    Complete first conditional

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,16 @@
 puts "Welcome to our company! Please answer the following questions."
 puts "What is your name?"
-  name = gets.chomp
+name = gets.chomp
 puts "Hi, #{name}! How old are you?"
-  age = gets.chomp.to_i
+age = gets.chomp.to_i
 puts "What year were you born?"
-  birth_year = gets.chomp.to_i
+birth_year = gets.chomp.to_i
 puts "#{name}, our company cafeteria serves garlic bread. Should we order some for you?"
-  order_bread = gets.chomp
+wants_bread = gets.chomp
 puts "Lastly, #{name}, would you like to enroll in the company's health insurance?"
-enroll_insurance = gets.chomp
+wants_insurance = gets.chomp
+
+if (birth_year + age == 2016 || birth_year + age == 2017) && wants_bread == "yes" || wants_insurance == "yes"
+result = "Probably not a vampire."
+end
+
