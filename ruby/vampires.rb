26e664c1fcc9b1830ef2dda28e1247e90097d478
genoth <nothgwynne@gmail.com>
2017-05-11 13:09:01 -0500

    Update new employee survey to process multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,35 +1,41 @@
-puts "Welcome to our company! Please answer the following questions."
-puts "What is your name?"
+puts "Hello! How many employees will be processed today?"
+employees = gets.chomp.to_i
+employees_processed = 0
+until employees_processed == employees
+  puts "Welcome to our company! Please answer the following questions."
+  puts "What is your name?"
   name = gets.chomp
-puts "Hi, #{name}! How old are you?"
+  puts "Hi, #{name}! How old are you?"
   years_old = gets.chomp.to_i
-puts "What year were you born?"
+  puts "What year were you born?"
   birth_year = gets.chomp.to_i
-if birth_year + years_old == 2016 || birth_year + years_old == 2017
-  age = TRUE
-end
-puts "#{name}, our company cafeteria serves garlic bread. Should we order some for you? Please answer yes or no."
-if gets.chomp == "yes"
-  wants_bread = TRUE
-end
-puts "Lastly, #{name}, would you like to enroll in the company's health insurance? Please answer yes or no."
-if gets.chomp == "yes"
-wants_insurance = TRUE
-end
+  if birth_year + years_old == 2016 || birth_year + years_old == 2017
+    age = true
+  end
+  puts "#{name}, our company cafeteria serves garlic bread. Should we order some for you? Please answer yes or no."
+  if gets.chomp == "yes"
+    wants_bread = true
+  end
+  puts "Lastly, #{name}, would you like to enroll in the company's health insurance? Please answer yes or no."
+  if gets.chomp == "yes"
+    wants_insurance = true
+  end
 
-if age && (wants_bread || wants_insurance)
-  result = "Probably not a vampire."
+  if age && (wants_bread || wants_insurance)
+    result = "Probably not a vampire."
+  end
+  if !age && (!wants_bread || !wants_insurance)
+    result = "Probably a vampire!"
+  end
+  if !age && !wants_bread && !wants_insurance
+    result = "Almost certainly a vampire!"
+  end
+  if name == "Drake Cula"
+    result = "DEFINITELY a vampire."
+  end
+  if name == "Tu Fang"
+    result = "DEFINITELY a vampire."
+  end
+  puts result
+  employees_processed = employees_processed + 1
 end
-if !age && (!wants_bread || !wants_insurance)
-  result = "Probably a vampire!"
-end
-if !age && !wants_bread && !wants_insurance
-  result = "Almost certainly a vampire!"
-end
-if name == "Drake Cula"
-  result = "DEFINITELY a vampire."
-end
-if name == "Tu Fang"
-  result = "DEFINITELY a vampire."
-end
-puts result
