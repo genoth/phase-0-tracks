c7f0bc833ee158bd17d38f29b47276147281e519
genoth <nothgwynne@gmail.com>
2017-05-17 11:17:47 -0500

    Complete Release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,7 +1,52 @@
-def whats_for_dinner
+# Release 0
+  def whats_for_dinner
   puts "It's almost dinnertime."
   yield("steak", "potatoes")
   puts "I hope it's delicious!"
 end
 
 whats_for_dinner { |dish1, dish2| puts "For dinner tonight, we're having #{dish1} and #{dish2}!" }
+
+# Release 1
+# 1
+# array days_of_week
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+# hash dinner_schedule
+dinner_schedule = {
+  monday: "steak and potatoes",
+  tuesday: "tacos",
+  wednesday: "tuna casserole",
+  thursday: "vegetable lasagne",
+  friday: "pizza"
+}
+
+# 2
+# iterate through array with .each
+puts "Exercise: Iterate through days_of_week array with .each. Original array is:"
+p days_of_week
+
+days_of_week.each do |day|
+  p "I work on #{day}."
+end
+
+puts "After the .each call, the array is:"
+p days_of_week
+
+# iterate through array with .map!
+puts "Exercise: Iterate through days_of_week array with .map! Original array is:"
+p days_of_week
+
+days_of_week.map! {|day| p day + " is the best day!"}
+
+puts "After the .map! call, array is:"
+p days_of_week
+
+# iterate through hash with .each
+puts "Exercise: Iterate through hash with .each. Original hash is:"
+p dinner_schedule
+
+dinner_schedule.each {|day, dinner| p "On #{day.upcase}, dinner will be #{dinner}."}
+
+puts "After the .each call, hash is:"
+p dinner_schedule
+
