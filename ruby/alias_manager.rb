bb4b155b01f5f4b4c2c98f36c8cb1881b16efeb8
genoth <nothgwynne@gmail.com>
2017-05-20 18:43:52 -0500

    Complete Release 1 user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,92 +1,67 @@
 # Release 0
-# Write a method that takes a full name as a string
-# Use .split and .reverse to swap first and last name
-# Create vowel key (alphabetized string of vowels aeiou)
-# Define next_vowel method
-# Create consonant key (alphabetized string of all consonants in the alphabet)
-# Use .delete to remove vowels and update consonant key
-# Use .split to convert both vowel and consonant keys to arrays?
-# Use .downcase to account for uppercase letters and use .capitalize to convert back to proper case
-# Write a method that checks to see if a letter in name_array exists in consonant_key_array, and if it does, returns the .next value from consonant_key_array. It not, returns the .next value from vowel_key_array, else returns " ".
+# The Alias problem will require 3 methods
 
-consonant_key = "abcdefghijklmnopqrstuvwxyz"
-consonant_key = consonant_key.delete("aeiou")
-consonant_key = consonant_key.chars
-# => ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+# 1 Letter Array method
+# Write a method that takes a full name as a string, makes all the letters lowercase, turns each name into an item of an array, and reverses the two items to form a new array (last name, first name), and then returns an array of all the characters of that array.
 
-vowel_key = "aeiou".chars
-# => ["a", "e", "i", "o", "u"]
+# 2 Encrypt Each Letter in the Array method
+# Write a method that establishes a vowel key and a consonant key, takes an array of characters, and checks if each one is a vowel, consonant, or space and then replaces the chracter with the next vowel in the key, next consonant in the key, or a space, respectively, based on the type. Edge cases will be "u" and "z". We can use a conditional to check for edge cases and return "a" and "b", respectively, for these edge cases. Returns a new character array.
 
-# puts vowel_key
-# puts vowel_key[1]
-# puts vowel_key.index("e")
-
-
-# puts vowel_key.index(target[1])
-# target.each do {|word| puts letter.index}
-
-
-# if vowel_key.include?(target[1])
-#  location = vowel_key.index((target[1]))
-#  puts location
-# end
+# 3
+# Create a method that takes a character array, joins the characters, and then splits them into 2 array items by using the space in between. Then, turns each array item into a string, and capitalizes each string. Then, adds a space before each item in the array. Then, adds a space before each string, and then joins the 2 strings. Then, removes the space before the first name.
 
 # puts "Thank you for using the aliasinator! Tell us your name and we'll find you an alias."
 
 def letter_array(name)
   names_swapped = name.downcase.split(' ').reverse
-  spaces_to_add = (names_swapped.length - 1)
-  names_swapped = names_swapped.insert(spaces_to_add, " ")
-  array_of_chars = names_swapped.join.chars
+  array_of_chars = names_swapped.join(' ').chars
 end
 
 def letter_encryptor(name)
-  target_array = letter_array(name)
   vowel_key = "aeiou".chars
-    consonant_key = "abcdefghijklmnopqrstuvwxyz"
-    consonant_key = consonant_key.delete("aeiou")
-    consonant_key = consonant_key.chars
-    target_array.map! do |letter|
-      if vowel_key.include?(letter)
-        if vowel_key == "u"
-          new_letter = "a"
-        else
-          location = vowel_key.index(letter)
-          new_letter = vowel_key[location + 1]
-        end
-      elsif consonant_key.include?(letter)
-        if consonant_key == "z"
-          new_letter = "b"
-        else
-          location = consonant_key.index(letter)
-          new_letter = consonant_key[location + 1]
-        end
-      else new_letter = " "
+  consonant_key = "abcdefghijklmnopqrstuvwxyz"
+  consonant_key = consonant_key.delete("aeiou")
+  consonant_key = consonant_key.chars
+  target_array = letter_array(name)
+  target_array.map! do |letter|
+    if vowel_key.include?(letter)
+      if vowel_key == "u"
+        new_letter = "a"
+      else
+        location = vowel_key.index(letter)
+        new_letter = vowel_key[location + 1]
       end
+    elsif consonant_key.include?(letter)
+      if consonant_key == "z"
+        new_letter = "b"
+      else
+        location = consonant_key.index(letter)
+        new_letter = consonant_key[location + 1]
+      end
+    else new_letter = " "
     end
+  end
 end
 
 def aliasinator(name)
   array = letter_encryptor(name)
   string = array.join
   separated_names = string.split(' ')
-  separated_names.map! do |item|
+  capitalized_names = separated_names.map do |item|
     item.to_s.capitalize
   end
-  separated_names.map! do |item|
-    item.insert(0, " ")
-  end
-  output = separated_names.join
-  output = output.slice(1,output.length)
+  output = capitalized_names.join(' ')
 end
 
-p aliasinator("Felicia Torres")
+puts "Hello! Welcome to the aliasinator. Please enter a name and we'll give you an alias!"
+name = gets.chomp
+puts "Your alias for #{name} is #{aliasinator(name)}. Press any key to continue using the aliasinator. If you're done, just type quit."
+while gets.chomp != "quit"
+  puts "Please enter a name and we'll give you an alias!"
+  name = gets.chomp
+  puts "Your alias is #{aliasinator(name)}. Press any key to continue using the aliasinator. If you're done, just type quit."
+end
+puts "Thank you. Goodbye!"
 
 
 
-
-# can we turn the result into an array?
-
-# run letter cryptometer on each item of the array
-# return new array of alias names ["vussit", "gimodoe"]
-# print array items as strings with spaces in between, and capitalize
