95eaa6622a8270357c4f8d2f57cfd510d3c28300
genoth <nothgwynne@gmail.com>
2017-05-21 18:04:14 -0500

    Complete hash drill item 2

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -123,12 +123,20 @@ puts output.join(' * ')
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
 # ----
+output = {}
+extinct_animals.each do |key, value|
+  if value < 2000
+    output[key] = value
+  end
+end
+p output
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
 # ----
+
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
