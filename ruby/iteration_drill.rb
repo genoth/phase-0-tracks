b434037d9427effdb7df49260288a25e1f74241f
genoth <nothgwynne@gmail.com>
2017-05-21 14:23:41 -0500

    Complete first drill, and second drill, does not address edge case

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -1,15 +1,40 @@
 # Array Drills
 
-zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
-                              "shotgun", "compass", "CB radio", "batteries"]
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "batteries"]
 
 # 1. Iterate through the zombie_apocalypse_supplies array,
 # printing each item in the array separated by an asterisk
 # ----
+zombie_apocalypse_supplies.each do |item|
+  puts item + "*"
+end
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
 # ----
+def runs_pass(input)
+  corrections_made = false
+  (input.length - 1).times do |index|
+    if input[index] > input[index+1]
+      corrections_made = true
+      val_0 = input[index]
+      val_1 = input[index + 1]
+      input[index] = val_1
+      input[index + 1] = val_0
+    end
+  end
+  return corrections_made
+end
+
+def bubble_sort(input)
+  until runs_pass(input) == false
+  end
+  return input
+end
+
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "batteries"]
+input = zombie_apocalypse_supplies
+p bubble_sort(input)
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
@@ -26,21 +51,20 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # and their supplies below. You should get rid of any duplicate items.
 # Find the built-in method that helps you accomplish this in the Ruby
 # documentation for Arrays.
-other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
-                            "solar battery", "flashlight"]
+# other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove","solar battery", "flashlight"]
 # ----
 
 # Hash Drills
 
-extinct_animals = {
-  "Tasmanian Tiger" => 1936,
-  "Eastern Hare Wallaby" => 1890,
-  "Dodo" => 1662,
-  "Pyrenean Ibex" => 2000,
-  "Passenger Pigeon" => 1914,
-  "West African Black Rhinoceros" => 2011,
-  "Laysan Crake" => 1923
-}
+# extinct_animals = {
+#   "Tasmanian Tiger" => 1936,
+#   "Eastern Hare Wallaby" => 1890,
+#   "Dodo" => 1662,
+#   "Pyrenean Ibex" => 2000,
+#   "Passenger Pigeon" => 1914,
+#   "West African Black Rhinoceros" => 2011,
+#   "Laysan Crake" => 1923
+# }
 
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
