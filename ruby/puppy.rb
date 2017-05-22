62ef55a6abf55753e2190b6e9b7853ac5559d2a3
genoth <nothgwynne@gmail.com>
2017-05-22 13:11:53 -0500

    Add puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,27 @@
+# PUPPY CLASS ----------------
+
+# CHARACTERISTICS -----------------
+# Age: varies
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+# Weight: varies
+
+# BEHAVIOR ----------------------
+# Sit
+# Explore
+# Lick
+# Nip
+# Bark
+# Cry
+# Potty
+# Eat
+# Chew
+
+class Puppy
+end
+
+duchess = puppy.new
+spot = puppy.new
+fido = puppy.new
