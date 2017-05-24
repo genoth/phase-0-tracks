9a530c3e132014787b0333530cf40c1bdac2c52a
genoth <nothgwynne@gmail.com>
2017-05-24 18:12:39 -0500

    Add attributes to santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,10 @@
 class Santa
 
-  def initialize(name, gender)
+  def initialize(name, gender, ethnicity)
+    #initialize method runs any time we call .new on the reindeer class!
     @name = name
     @gender = gender
+    @ethnicity = ethnicity
     @reindeer_preference = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
     puts "Initializing Santa instance..."
@@ -16,12 +18,19 @@ class Santa
 end
 
 # DRIVER CODE
-mall_santa = Santa.new("prefer not to say", "prefer not to say")
+mall_santa = Santa.new("Marge", "prefer not to say", "prefer not to say")
 mall_santa.speak
 mall_santa.eat_milk_and_cookies("sugar cookie")
 
 santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
+santas << Santa.new("Anna", "agender", "black")
+santas << Santa.new("Bella", "female", "Latino")
+santas << Santa.new("Charlie", "bigender", "white")
+santas << Santa.new("Dennis", "male", "Japanese")
+santas << Santa.new("Emily", "female", "prefer not to say")
+santas << Santa.new("Fareed", "gender fluid", "Mystical Creature")
+santas << Santa.new("Genevieve", "N/A", "N/A")
+
+p santas
+
 
