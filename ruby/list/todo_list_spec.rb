7fba2052ecd566afe07587f72bfec6a08ac5c417
genoth <nothgwynne@gmail.com>
2017-05-25 12:57:44 -0500

    Separat class into its own file, add require_relative code to spec file

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,25 +1,4 @@
-class TodoList
-  def initialize(list)
-    @list = list
-  "Initializing new list"
-  end
-
-  def get_items()
-    return @list
-  end
-
-  def get_item(integer)
-    return @list[integer]
-  end
-
-  def add_item(new_item)
-    return @list << new_item
-  end
-
-  def delete_item(item)
-    return @list.delete(item)
-  end
-end
+require_relative 'class'
 
 describe TodoList do
   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
