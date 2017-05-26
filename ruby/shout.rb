1543ac75294ef41c3692cd6341352d275628fe3b
genoth <nothgwynne@gmail.com>
2017-05-26 14:39:18 -0500

    Complete 6.4 Release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,42 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words + " wooooo!!"
+#   end
+# end
+
+# # Driver Code
+# puts Shout.yell_angrily("Nooooooooo")
+# puts Shout.yelling_happily("Yes I did it yaaay")
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
-    words + "wooooo!!"
+  def yell_happily(words)
+    words + " wooooo!!"
   end
 end
 
-# Driver Code
-puts Shout.yell_angrily("Nooooooooo")
-puts Shout.yelling_happily("Yes I did it ")
+class Regular_person
+  include Shout
+end
+
+class Band
+  include Shout
+end
+
+Otis_Day_and_the_Knights = Band.new
+Teacher = Regular_person.new
+
+
+# DRIVER CODE
+puts Teacher.yell_angrily("Detention")
+puts Teacher.yell_happily("Great job on the test")
+
+puts Otis_Day_and_the_Knights.yell_happily("You know you make me wanna shout")
+puts Otis_Day_and_the_Knights.yell_angrily("This song requires participation")
