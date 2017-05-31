76da59b79e1db63ed41568a4ebb91b4989b8c98f
genoth <nothgwynne@gmail.com>
2017-05-30 19:32:21 -0500

    Put runner code into separate runner file

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -10,28 +10,8 @@ require_relative 'game'
   # 'it' block for each individual test
     # expect(<YOUR CODE>).to eq <RESULT>
 
-require_relative 'calculator'
-
 describe WordGame do
-  let(:game) { Game.new }
-
-  it "adds two integers" do
-    expect(calculator.add(3,4)).to eq 7
-  end
-
-  it "subtracts two integers" do
-    expect(calculator.subtract(7,1)).to eq 6
-  end
-
-  it "multiplies two integers" do
-    expect(calculator.multiply(2,3)).to eq 6
-  end
-
-  it "divides two integers" do
-    expect(calculator.divide(20,5)).to eq 4
-  end
-
-  it "divides two integers" do
-    expect(calculator.divide(20,5)).to be_a_kind_of(Integer)
+  it "works" do
+    WordGame.new("foobar")
   end
 end
