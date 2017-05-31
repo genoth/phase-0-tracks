6715bd936a4dbbc9cfd8fb83bfb44bcb6c8283b2
genoth <nothgwynne@gmail.com>
2017-05-30 19:55:08 -0500

    Create test file and initial tests

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -10,8 +10,32 @@ require_relative 'game'
   # 'it' block for each individual test
     # expect(<YOUR CODE>).to eq <RESULT>
 
+
 describe WordGame do
-  it "works" do
-    WordGame.new("foobar")
+  it "sets secret word on initialize" do
+    game = WordGame.new("foobar")
+    expect(game.secret_word).to eq("foobar")
   end
+
+  it "has a secret_letter_array" do
+    game = WordGame.new("foobar")
+    expect(game.secret_letter_array).to eq(["f", "o", "o", "b", "a", "r"])
+  end
+
+  it "assigns a maximum amount of guesses" do
+    game = WordGame.new("foobar")
+    expect(game.guesses_allowed).to eq(game.secret_word.length)
+  end
+
+  it "creates a masked array" do
+    game = WordGame.new("foobar")
+    expect(game.masked_array).to eq(["_", "_", "_", "_", "_", "_"])
+  end
+
+  it "determines if the game is won" do
+    game = WordGame.new("foobar")
+    expect(game.game_won?).to eq(false)
+  end
+
+
 end
