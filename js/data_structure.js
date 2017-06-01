57ff009f0c7a04a51efd10274083f1f1e8dc4086
genoth <nothgwynne@gmail.com>
2017-06-01 11:06:53 -0500

    Complete Release 1, 7.2

---
+++ js/data_structure.js
@@ -0,0 +1,18 @@
+var horseNames=["Ed","Sea Biscuit","Black Beauty","Bob"];
+var horseColors=["blue", "red","yellow","green"];
+​
+horseColors.push("purple");
+horseNames.push("Buttercup");
+​
+function pair(horseNames,horseColors){
+var horses={};
+for(i=0;i<horseNames.length;i++){
+ horses[names[i]]=colors[i]
+}
+​
+for(i=0;i<horseColors.length;i++){
+ console.log(horseColors[i])
+}
+}
+​
+console.log(pair(horseNames,horseColors));
