99671969c99d357b2f1ebb0d6d93680a32e4b9aa
genoth <nothgwynne@gmail.com>
2017-06-04 23:52:32 -0500

    Update pseudocode for release 1 and 2

--- js/algos.js
+++ js/algos.js
@@ -14,20 +14,24 @@ function longestPhrase(Array){
 }
 
 // ---------------------Release 1---------------------
+// Pseudocode
+// Create a method that takes 2 objects as arguments. It then takes a key that exists in the first object, and prints the value associated with this key, for both objects. It then compares them, if the same, returns true. If not, continues loop with the next key in the first object, prints the value associated with this key for both objects. Compares them, if the same, returns true. If not, returns false.
 
-var x;
-for (x in object1) {
-  a = (x, object1[x]);
-  b = (x, object2[x]);
-}
 
 function equality(objectA, objectB) {
   var x;
   for (x in objectA) {
-    var a = (x, objectA[x]);
-    var b = (x, objectB[x]);
+    // sets value of X to be a key in objectA
+    var a = objectA[x];
+    // 1st loop thru- a = value from key X for objectA
+    // 2nd loop thru - a = resets X to be the second key. value from new key X for objectA
+    var b = objectB[x];
+    // 1st loop thru- b = value from key X for objectB
+    // 2nd loop thru - b = value from new key X pair for objectB
     if (a == b) {
       return true
+    // 1st lopo thru - is a = b? in this case, it's looking at the key name, a = Steven, b = Tamir. no, they don't match.
+    // 2nd loop thru - is a = b? in this case, it's looking at the key age, a = 54, b = 54, so yes, true.
     }
   }
 }
@@ -44,20 +48,15 @@ var object4 = {animal: "Dog", legs: 3};
 // }
 
 // -------------------Release 2---------------------
+// Pseudocode
+// First function - takes 2 numbers as arguments, generates a random integer from a range, using the number arguments as minimum and maximum values. The function will be used to determine the length of each random word in the array.
+// Second function, takes no argument. Sets value text to equal "". We will be adding individual characters to this string. Establishes possible characters to be used (i.e. the alphabet) to generate the string. Uses the First function to generate a random integer, and iterates over the string of possible characters, and generates a random character, that number of times, adding the resulting character to the string. Returns the string.
+// Third function, takes an integer as an argument. This integer determines the number of items in an array of randomly generated strings. Sets array to equal an empty array, and then runs the second function and adds the generated string to the array to be returned. Runs the second function n times as indicatd by the integer in the argument.
+
 function randomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min))
-}
-
-function generateString()
-{
-  var text = "";
-  var possible = "abcdefghijklmnopqrstuvwxyz";
-
-  for( var i=0; i < (randomInt(1, 11)); i++)
-    text = text + possible.charAt(Math.floor(Math.random() * possible.length));
-  return text;
 }
 
 function generateString1()
@@ -88,10 +87,10 @@ function generateArray(int)
 
 // -----Release 1
 
-// if ((equality(object1, object2)) == true) {
-//   console.log(true)
-// }
-// console.log(equality(object3, object4))
+if ((equality(object1, object2)) == true) {
+  console.log(true)
+}
+console.log(equality(object3, object4))
 
 // -----Release 2
 // console.log(generateArray(4))
