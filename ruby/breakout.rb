a0b973f48f8862e7b5db593c1121a26e873920b0
genoth <nothgwynne@gmail.com>
2017-06-04 21:10:01 -0500

    Complete Release 3 for breakout session

--- ruby/breakout.rb
+++ ruby/breakout.rb
@@ -12,13 +12,34 @@ class Task_list
     if @locations.include?(new_location)
       puts "#{new_location} is already on the list."
     else
-      @locations[new_location] = ""
+      @locations[new_location] = []
     end
   end
+
+  def add_task(new_task, location)
+    if @locations.include?(location) && @locations[location].include?(new_task)
+      puts "The task #{new_task} is already on the list for the location #{location}"
+    elsif @locations.include?(location)
+      # check if task already exists at that location
+      # if not, puts
+      @locations[location] << [new_task]
+    else # if it's a totally new place...
+      @locations[location] = [new_task]
+    end
+  end
+
 end
 
 first_list = Task_list.new("Friday", "Geoffrey")
 first_list.add_location("Walgreens")
+
+first_list.add_task("pick up groceries", "Marianos")
+p first_list
+
+first_list.add_task("pick up groceries", "Walgreens")
+first_list.add_task("buy flowers", "Marianos")
+
+first_list.add_task("pick up groceries", "Marianos")
 p first_list
 
 
