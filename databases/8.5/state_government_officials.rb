55501ec0254a52dd793ff31a58e3cd51d70fc91e
genoth <nothgwynne@gmail.com>
2017-06-10 17:13:34 -0500

    Successfully create db tables using ruby file

--- databases/8.5/state_government_officials.rb
+++ databases/8.5/state_government_officials.rb
@@ -6,24 +6,24 @@ db.results_as_hash = true
 create_state_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS states(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
+    name VARCHAR(255)
   )
 SQL
 
 create_party_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS parties(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
+    name VARCHAR(255)
     )
 SQL
 
 create_government_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS government_officials(
+  CREATE TABLE IF NOT EXISTS officials(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
     position VARCHAR(255),
-    party_id INTEGER, FOREIGN KEY(party_id) REFERENCES parties(id),
-    state_id INTEGER, FOREIGN KEY(state_id) REFERENCES states(id)
+    state VARCHAR(255),
+    party_id INTEGER, FOREIGN KEY(party_id) REFERENCES parties(id)
   )
 SQL
 
