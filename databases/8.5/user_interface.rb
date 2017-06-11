53ff6aa0f4b4f407a5975abe3a3c5c1a8ea1c8c3
genoth <nothgwynne@gmail.com>
2017-06-10 21:33:28 -0500

    Refactor to only make one call to the database

--- databases/8.5/user_interface.rb
+++ databases/8.5/user_interface.rb
@@ -2,38 +2,19 @@ require 'sqlite3'
 require_relative './state_government_officials.rb'
 db = SQLite3::Database.open("state_officials.db")
 
-####################
-# explore ORM by retrieving data
-
-## experiment with format of printed data
 def retrieve_info(db, input)
-  name = (db.execute("SELECT officials.name FROM officials WHERE state = '#{input}';")).first.first
-  party = (db.execute("SELECT parties.name FROM officials JOIN parties ON officials.party_id = parties.id WHERE state = '#{input}';")).first.first
-  puts "The attorney general for #{input} is #{name}, of the #{party} party."
+  row = db.execute("SELECT officials.name, officials.position, parties.name FROM officials JOIN parties ON officials.party_id = parties.id WHERE state = '#{input}';").first
 end
-    # officials.position, parties.name FROM officials JOIN parties ON officials.party_id = parties.id WHERE state = '#{input}';")
 
-
-### THIS WORKS!
-# def retrieve_info(db, input)
-#   db.execute("SELECT officials.name, officials.position, parties.name FROM officials JOIN parties ON officials.party_id = parties.id WHERE state = '#{input}';")
-# end
 puts "Welcome to the state government officials database."
 puts "To find information about a state's Attorney General, please type the state name."
 state = gets.chomp
 
-# puts db.execute("SELECT * FROM officials")
+# pull row and parse data into human readable format.
+row = retrieve_info(db, state)
+name = row[0]
+position = row[1]
+party = row[2]
+puts "The #{position} for #{state} is #{name}, of the #{party} party."
 
-puts retrieve_info(db, state)
-
-
-# officials = db.execute("SELECT officials.*, officials.name, officials.position, officials.party_id, parties.name WHERE state = #{input}")
-#   puts "#{officials['name']} is #{officials['position']} for the state of #{officials['state']}."
-
-# retrieve_data_cmd = <<-SQL
-# SELECT officials.*, parties.name FROM officials INNER JOIN parties ON officials.party_id = parties.id WHERE officials.state = "Illinois"
-# )
-# # SQL
-
-# db.execute(retrieve_data_cmd)
 
