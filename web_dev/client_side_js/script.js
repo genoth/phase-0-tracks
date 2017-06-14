0efe6094daad648f1eb826004eafaf844bbf7596
genoth <nothgwynne@gmail.com>
2017-06-14 13:24:20 -0500

    Complete Release 2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -3,12 +3,31 @@
 console.log("this works!")
 
 var photo = document.getElementById("photo");
-  photo.hidden = true;
+  photo.hidden = false;
   console.log(photo);
   photo.style.border = "3px solid green";
 
 var header = document.getElementsByTagName("h1");
   header[0].style.color = "pink";
+
+// Add an event listener to your client_side_js project that delays the execution of some snippet of code until the user clicks or takes some other action. Keep it simple with a console.log call, or get fancy by dramatically revealing an animated GIF -- it's up to you.
+
+function change_border(event) {
+  console.log(event);
+  event.target.style.border = "15px solid red";
+}
+
+function change_color(event) {
+  console.log(event);
+  event.target.style.color = "pink";
+}
+
+var link = document.getElementsByTagName("a");
+  console.log(link[3]);
+  link[3].addEventListener("click", change_color);
+
+
+photo.addEventListener("click", change_border);
 
 
 
